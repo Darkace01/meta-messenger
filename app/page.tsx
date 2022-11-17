90033349f95ddd2d07bcb63e2172f5d9206c85d4
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-11-17 08:30:22 +0100

    chatinput component and message list template

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import ChatInput from './ChatInput';
 import Header from './Header';
+import MessageList from './MessageList';
 
 function HomePage() {
   return (
     <main>
       <Header />
+      <MessageList />
+      <ChatInput />
     </main>
   );
 }
